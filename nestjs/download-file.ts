d3247cc7764d131b2473558aacd6531eb663a4fd
Sabbir Siddiqui <sabbir.m.siddiqui@gmail.com>
2023-02-23 20:19:24 +0600

    Update download-file.ts

--- nestjs/download-file.ts
+++ nestjs/download-file.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Injectable, Res } from '@nestjs/common';
 import { Response } from 'express';
 import * as xlsx from 'xlsx';
 
+// utility to download json as excel from controller
 function downloadJsonAsExcelFile(
   jsonData: any[],
   fileName: string,
@@ -23,10 +24,14 @@ function downloadJsonAsExcelFile(
   res.send(buf);
 }
 
-@Injectable()
-export class DownloadService {
-  async getData() {
-    return [
+// sample controller
+@Controller('download')
+export class DownloadController {
+  constructor(private readonly downloadService: DownloadService) {}
+
+  @Get('/json')
+  async download(@Res() res: Response) {
+    const data = [
       {
         name: 'John',
         title: 'EM',
@@ -36,16 +41,74 @@ export class DownloadService {
         title: 'SDE',
       },
     ];
-  }
-}
-
-@Controller('download')
-export class DownloadController {
-  constructor(private readonly downloadService: DownloadService) {}
-
-  @Get('/')
-  async download(@Res() res: Response) {
-    const data = await this.downloadService.getData();
     return downloadJsonAsExcelFile(data, 'your-filename.xlsx', res);
   }
 }
+
+// util to parse downloaded file to json
+export async function getExcelFromServerDownload(
+  server: HttpServer,
+  opts: {
+    url: string;
+    method: 'get' | 'post';
+    headers: any;
+  },
+) {
+  const { url, method, headers } = opts;
+  const response = await request(server)
+    [method](url)
+    .buffer()
+    .parse((res, callback) => {
+      res.setEncoding('binary');
+      (res as any).data = '';
+      res.on('data', (chunk) => {
+        (res as any).data += chunk;
+      });
+      res.on('end', () => {
+        callback(null, Buffer.from((res as any).data, 'binary'));
+      });
+    })
+    .set(headers);
+
+  const data = XLSX.read(response.body, {
+    type: 'buffer',
+  });
+
+  const json = XLSX.utils.sheet_to_json(data.Sheets[data.SheetNames[0]]);
+
+  return json;
+}
+
+
+// e2e test case
+describe('Test Download', () => {
+  let server;
+  
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, HttpModule],
+      providers: [IntegrationTestUtils],
+    }).compile();
+
+    const app = moduleFixture.createNestApplication();
+
+    server = app.getHttpServer();
+  })
+  
+  it('should download the file', async () => {
+    const response = await getExcelFromServerDownload(server, {
+      url: '/download/json',
+      method: 'get',
+    });
+    expect(response).toEqual([
+      {
+        name: 'John',
+        title: 'EM',
+      },
+      {
+        name: 'Jane',
+        title: 'SDE',
+      },
+    ]);
+  });
+})
